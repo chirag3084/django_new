1700a3e646b96996992f967f711c40cdad3d226e
Surti Chirag <chiragsurti308444@gmail.com>
2025-08-30 18:12:10 +0530

    error fix

--- app/manager.py
+++ app/manager.py
@@ -6,7 +6,7 @@ class UserManager(BaseUserManager):
         if not phone_number:
             raise ValueError("Phone number is required")
 
-        extra_fields["email"] = self.normalize_email(extra_fields["email"])
+        # extra_fields['email'] = self.normalize_email(extra_fields['email'])
         user = self.model(phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, phone_number, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
 
         return self.create_user(phone_number, password, **extra_fields)

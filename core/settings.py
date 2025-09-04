132eb78285d288b0fa3996b0b6223b852bb8222d
cody <surtivatsal23@gmail.com>
2025-09-04 16:06:55 +0530

    Update SECRET_KEY to use environment variable

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e5kk(q=m^vu1pj^jpy054-4=qn3hn)l#$o6p(diuy4t!q-f7y="
+SECRET_KEY = "os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
